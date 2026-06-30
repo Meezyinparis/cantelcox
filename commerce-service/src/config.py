e9d4e84d983a983f73e6f9a4a36d77aa49754953
Emmanuel Senosier <98911028+Meezyinparis@users.noreply.github.com>
2026-06-29 20:00:20 -0400

    fix

--- commerce-service/src/config.py
+++ commerce-service/src/config.py
@@ -7,7 +7,7 @@ DB_HOST = os.getenv("COMMERCE_DB_HOST")
 DB_PORT = int(os.getenv("COMMERCE_DB_PORT"))
 DB_NAME = os.getenv("COMMERCE_DB_NAME")
 DB_USER = os.getenv("COMMERCE_DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PASSWORD = os.getenv("COMMERCE_DB_PASSWORD")
 
 REDIS_HOST = os.getenv("REDIS_HOST")
 REDIS_PORT = int(os.getenv("REDIS_PORT"))
